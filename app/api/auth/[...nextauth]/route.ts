65f18ecaf74a3ab7984a2b39d0a44ec14fc8f641
Junais Babu <junais@Junaiss-MacBook-Air.local>
2023-11-13 22:47:59 +0530

    set-up the CredentialsProvider

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import { users } from "@/helpers/constants";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", placeholder: "Enter Email" },
+        password: { label: "Password", placeholder: "Password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials || credentials.email || credentials.password) {
+          return null;
+        }
+        const user = users.find((user) => user.email === credentials.email);
+        if (user?.password === credentials.password) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
